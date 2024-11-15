45c64ab0170c79393a5ec300eacca1ed87f579f5
kunal528 <64953335+kunal528@users.noreply.github.com>
2024-11-15 22:21:30 +0700

    Add @privy-io/react-auth and react-router-dom dependencies

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useDelegatedActions, usePrivy } from '@privy-io/react-auth';
+import { useEffect } from 'react';
 
+
+// wallet-api:MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgk0LBNgq1EVNSzxkRIak3eybq3efJXnoPMbkKwtir12ahRANCAAT5Do9vYFL4B18W6nRrMe4BgPe63LSYe9escs3ujif3huc8u5rKGqfvRj4vFkghjzvl4DYwesgACLSvYgUOFlu5
 function App() {
+
+  const { login, user, authenticated } = usePrivy();
+  const { delegateWallet } = useDelegatedActions();
+  useEffect(() => {
+    if (user?.wallet && !user?.wallet?.delegated) {
+      console.log("User", user);
+      delegateWallet({
+        address: user.wallet.address,
+        chainType: user.wallet.chainType,
+      }).then(() => {
+        if (window.FlutterChannel) {
+          window.FlutterChannel.postMessage('closeWebView');
+        }
+      }).catch(console.error);
+    }
+    if (!authenticated)
+      login();
+  }, [user, user?.wallet]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+
+import { PrivyProvider } from '@privy-io/react-auth';
+
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <PrivyProvider
+        appId="cm3hh4seg003doz8rt0y0kt4i"
+        config={{
+          // Display email and wallet as login methods
+          loginMethods: ['sms'],
+          // Customize Privy's appearance in your app
+          appearance: {
+            theme: 'light',
+            accentColor: '#676FFF',
+            logo: 'https://i.imgur.com/ZZc9r4L.png',
+          },
+          // Create embedded wallets for users who don't have a wallet
+          embeddedWallets: {
+            showWalletUIs: true,
+            createOnLogin: "all-users",
+          },
+        }}
+      >
+        <App />
+      </PrivyProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
